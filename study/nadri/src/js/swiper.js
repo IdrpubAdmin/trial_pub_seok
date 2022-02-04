7d8b3bcfddb57825a4ecced26c13e9a46e6e91a6
eunseok <eunseok0722@gmail.com>
2022-02-04 17:59:32 +0900

    22.02.04. nadri 작업결과 - store.html, cs.html 반응형 작업 완료 - main.html swiper 기능구현 및 반응형 작업 진행 중

--- study/nadri/src/js/swiper.js
+++ study/nadri/src/js/swiper.js
@@ -1,3 +1,88 @@
+//main_menu_slide 스와이퍼 동작 구현
+const MainSwiper = new Swiper('.main_menu_slide', {
+    slidesPerView: 4.2,
+    spaceBetween: 20,
+    centeredSlides: false,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+            autoheight: true
+        },
+        641: {
+            slidesPerView: 3.2,
+            spaceBetween: 15,
+            autoheight: true
+        },
+        1199: {
+            slidesPerView: 4.2,
+            spaceBetween: 20,
+        },
+    }
+});
+
+//main_store_list 스와이퍼 동작 구현
+const MainStoreSwiper = new Swiper('.main_store_list', {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.5,
+            spaceBetween: 0,
+            autoheight: true
+        },
+        641: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            autoheight: true
+        },
+        1199: {
+            slidesPerView: 3,
+            spaceBetween: 0
+        },
+    }
+});
+
+//main_franchise_issue_contents 스와이퍼 동작구현
+const MainFranchiseSwiper = new Swiper('.main_franchise_issue_contents', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    direction: 'vertical',
+    autoheight: false,
+    // centeredSlides: false,
+    // loop: false,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+
+        },
+        641: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        1199: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+    }
+});
+
 
 
 //news_contents 스와이퍼 동작 구현
