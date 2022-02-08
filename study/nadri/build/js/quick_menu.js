a92b8d82c0d6ec092b17b5de58d4692f8bf6cb04
eunseok <eunseok0722@gmail.com>
2022-02-08 18:04:15 +0900

    nadri 22.02.08. 작업내용
    - 불필요한 파일 삭제
    - sourcemap 활성화, outstyle:compact 적용
    - 클래스명 하이픈으로 수정
    - 일부 요소 폰트 크기 오류 수정
    - 공통 디자인 클래스 공통화 작업 중 (menu, news 완료)
    - media query mixin 수정 중 (menu, news 완료)

--- study/nadri/build/js/quick_menu.js
+++ study/nadri/build/js/quick_menu.js
@@ -1,18 +1,18 @@
 const quickMenu = document.querySelector('.quick'),
-showItem = document.querySelectorAll('.show_item'),
-showTop = document.querySelector('.show_top');
+showItem = document.querySelectorAll('.show-item'),
+showTop = document.querySelector('.show-top');
 
 // .quick hover 동작 구현
 function activate() {
     for (i=0; i<showItem.length; i++) {
-    showItem[i].classList.add('quick_btn_info');}
-    showTop.classList.add('show_top_btn');
+    showItem[i].classList.add('quick-btn-info');}
+    showTop.classList.add('show-top-btn');
 };
 
 function deactivate() {
     for (i=0; i<showItem.length; i++) {
-        showItem[i].classList.remove('quick_btn_info');}
-    showTop.classList.remove('show_top_btn');
+        showItem[i].classList.remove('quick-btn-info');}
+    showTop.classList.remove('show-top-btn');
 };
 
 quickMenu.addEventListener("mouseover", activate);
